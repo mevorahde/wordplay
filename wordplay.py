940bc7329d5dc4708697f9b89cc38eca9c3d379c
David Mevorah <mevorahde@hotmail.com>
2019-02-22 03:54:34 -0800

    Add key and value list for scrabble point scores.

--- wordplay.py
+++ wordplay.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+scrabble_point_values = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+          "x": 8, "z": 10}
 
-def score_word(word):
+
+def score_word(word:str):
     score = 0
     for letter in word:
-        # add code here
-        pass
+        score += scrabble_point_values[letter]
     return score
+
+
+score_word("word")
+

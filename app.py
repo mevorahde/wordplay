ff6ddd06f778d2b4d90027331ac418c07501f203
David Mevorah <mevorahde@hotmail.com>
2019-03-19 16:42:41 -0700

    Update app.py to remove unused dummy test links

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ def hello():
     <input type="text" name="word">
     <input type="submit">
     </form>
-    <p>Try accessing the <a href="/api/v1/data">dummy data</a> endpoint.</p>
-    <p>Score letters <a href="/api/v1/score?l=wombat">'wombat'</a></p>
     """
+
 
 @app.route("/api/v1/data")
 def dummy_data():
@@ -31,7 +30,7 @@ def score_word():
     word_list = wordplay.word_list
     if lowercase_word in word_list:
         score = wordplay.score_word(lowercase_word)
-        result = {'letters': sorted(list(lowercase_word)),'score': score}
+        result = {'word': lowercase_word, 'letters': sorted(list(lowercase_word)),'score': score}
     else:
         result = "You did not enter a valid word."
     return jsonify(result)
